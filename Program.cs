2285cf95a2e4022ff84d315383cf2c26877ad9f4
kristo <kristo@hotmail.co.uk>
2020-01-17 17:22:08 +0000

    Initial

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceExample.Interfaces;
+
+namespace InterfaceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBankAccount savingAccount = new SavingAccount();
+            IBankAccount currentAccount = new CurrentAccount();
+
+            savingAccount.Deposit(50);
+            savingAccount.Withdraw(10);
+            savingAccount.ToString();
+
+            currentAccount.Deposit(100);
+            currentAccount.Withdraw(50);
+            currentAccount.Withdraw(10);
+            currentAccount.ToString();
+
+            Console.ReadLine();
+        }
+    }
+}
